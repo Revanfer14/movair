f0b37bb5d6b1c3571545bc26c63b5bd8cd1222a5
Revan Ferdinand <revancrozz@gmail.com>
2026-08-12 20:48:18 +0700

    first commit

---
+++ movair/movairApp.swift
@@ -0,0 +1,17 @@
+//
+//  movairApp.swift
+//  movair
+//
+//  Created by Revan Ferdinand on 12/08/26.
+//
+
+import SwiftUI
+
+@main
+struct movairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
